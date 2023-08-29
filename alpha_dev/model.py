36678694d9a150bcaa9ac843f771a7147076f3a4
Kye <kye@apacmediasolutions.com>
2023-08-28 20:32:56 -0400

    alpha dev

--- alpha_dev/model.py
+++ alpha_dev/model.py
@@ -1,2 +1,83 @@
-from alpha_dev.transformer import Transformer, AutoregressiveWrapper, Decoder
+from torch.nn import Module
 
+from alpha_dev.transformer import AutoregressiveWrapper, Decoder, Transformer
+
+
+class AlphaDev(Module):
+    """
+    AlphaDev is a transformer-based model architecture. It initializes with 
+    a Transformer and AutoregressiveWrapper with default or user-specified parameters.
+    Initialize the model with specified or default parameters.
+    Args:
+    - num_tokens: Number of tokens in the vocabulary
+    - max_seq_len: Maximum sequence length
+    - dim: Dimension of the model
+    - depth: Depth of the model
+    - dim_head: Dimension of the model head
+    - heads: Number of heads
+    - use_abs_pos_emb: Whether to use absolute position embedding
+    - alibi_pos_bias: Alibi position bias
+    - alibi_num_heads: Number of alibi heads
+    - rotary_xpos: Rotary position
+    - attn_flash: Attention flash
+    - deepnorm: Deep normalization
+    - shift_tokens: Number of tokens to shift
+    - attn_one_kv_head: Attention one key/value head
+    - qk_norm: Query-key normalization
+    - attn_qk_norm: Attention query-key normalization
+    - attn_qk_norm_dim_scale: Attention query-key normalization dimension scale
+    - embedding_provider: Embedding provider module
+    """
+    def __init__(self, 
+                 num_tokens=50432, 
+                 max_seq_len=8192, 
+                 dim=2560, 
+                 depth=32, 
+                 dim_head=128, 
+                 heads=24,
+                 use_abs_pos_emb=False, 
+                 alibi_pos_bias=True, 
+                 alibi_num_heads=12, 
+                 rotary_xpos=True,
+                 attn_flash=True, 
+                 attn_kv_heads = 2,
+                 qk_norm=True, 
+                 attn_qk_norm=True, 
+                 attn_qk_norm_dim_scale=True, 
+                 ):
+        super().__init__()
+
+        try:
+            self.decoder = Transformer(
+                num_tokens=num_tokens,
+                max_seq_len=max_seq_len,
+                use_abs_pos_emb=use_abs_pos_emb,
+                attn_layers=Decoder(
+                    dim=dim,
+                    depth=depth,
+                    dim_head=dim_head,
+                    heads=heads,
+                    alibi_pos_bias=alibi_pos_bias,
+                    alibi_num_heads=alibi_num_heads,
+                    rotary_xpos=rotary_xpos,
+                    attn_flash=attn_flash,
+                    attn_kv_heads=attn_kv_heads,
+                    qk_norm=qk_norm,
+                    attn_qk_norm=attn_qk_norm,
+                    attn_qk_norm_dim_scale=attn_qk_norm_dim_scale
+                )
+            )
+
+            self.decoder = AutoregressiveWrapper(self.decoder)
+
+        except Exception as e:
+            print("Failed to initialize Andromeda: ", e)
+            raise
+
+    def forward(self, text_tokens, **kwargs):
+        try:
+            model_input = self.decoder.forward(text_tokens)[0]
+            return self.decoder(model_input, padded_x=model_input[0])
+        except Exception as e:
+            print("Failed in forward method: ", e)
+            raise
